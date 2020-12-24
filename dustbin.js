4fb6c84b62ae097583096a094019c80b795710ca
arunabh689 <73931933+arunabh689@users.noreply.github.com>
2020-12-24 16:26:43 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,10 @@
+class dustbin {
+    constrouctor( x ,y ,w ,h ){
+        display(); {
+            var pos = this.body.position;
+            rectMode(CENTER);
+            fill("white");
+            rect(pos.x, pos.y, this.w, this.h);}
+
+    }
+}
